5d87135a0bcae2f2a2b485ad516fbe875867318f
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-03 00:54:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,18 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => { :patriarch => {},
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
    },
-      #your key/value pairs here!
-      }, 
-    :capulet => { :patriarch => {},
+   :capulet => {
+      :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
    }
 }
- 
-      #your key/value pairs here!
+  #your key/value pairs here!
 
 end
