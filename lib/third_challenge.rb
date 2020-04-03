bc0c20962dfe5249f11b521455f9cd49cdd845af
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-03 01:03:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,7 +19,7 @@ def third_challenge
       :matriarch => { name: "Lady Capulet", age: "51"
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => { name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
