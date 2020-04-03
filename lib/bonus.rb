20b6bd3b7121c79cc7935658a5ad5b5079c65026
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-03 01:49:49 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -26,9 +26,7 @@ epic_tragedy [:capulet] [:heroine] [:status] = "dead"
 
 puts epic_tragedy
 
-  
-
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  
 end
 
